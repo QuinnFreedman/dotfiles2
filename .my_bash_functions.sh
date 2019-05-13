ba009ea3094801303808746ddb0c1d96aa27a29c
Quinn Freedman <quinnfreedman@gmail.com>
2019-05-13 00:42:19 -0400

    added plug to setup

--- .my_bash_functions.sh
+++ .my_bash_functions.sh
@@ -1,1 +1,9 @@
+local PLUG_FILE=~/.config/nvim/autoload/plug.vim
+# PLUG_URL = "https://raw.githubusercontent.com/junegunn/vim-plug/518a3566c32cec35c68749c765127ef6f7703630/plug.vim"
+if [ ! -f "$PLUG_FILE" ]; then
+    echo "Vim Plug is not installed; Downloading..."
+    curl -fLso "$PLUG_FILE" --create-dirs \
+	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
 alias config='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
