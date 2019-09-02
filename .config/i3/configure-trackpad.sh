3ea1deb15c99013240e74617354d65d76c017867
Quinn Freedman <quinnfreedman@gmail.com>
2019-09-02 10:03:46 -0700

    i3

---
+++ .config/i3/configure-trackpad.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/zsh
+
+#xinput list
+#xinput list-props 13
+
+xinput set-prop 13 292	1
+xinput set-prop 13 294	1
+xinput set-prop 13 296	0
+xinput set-prop 13 298	1, 0
+xinput set-prop 13 300	1
+xinput set-prop 13 302	1
+xinput set-prop 13 305	1, 0, 0
+xinput set-prop 13 307	1, 1
+xinput set-prop 13 308	1, 0
+xinput set-prop 13 310	0
+xinput set-prop 13 312	0.785461
+xinput set-prop 13 317	1
+
