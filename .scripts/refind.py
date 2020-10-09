db018d6c93df7a6643fdf3b0894dd3362eeb18b1
Quinn Freedman <quinnfreedman@gmail.com>
2020-10-09 09:28:09 -0700

    Update refind.py

--- .scripts/refind.py
+++ .scripts/refind.py
@@ -6,6 +6,16 @@ import argparse
 import os
 from os.path import isdir, join
 
+
+def bool_input(prompt):
+    while True:
+        response = input(prompt + " [y/n]: ").lower().strip()
+        if response == "y" or response == "yes":
+            return True
+        if response == "n" or response == "no":
+            return False
+
+
 def get_filenames(path, recursive):
     if recursive:
         for (dirpath, dirnames, filenames) in os.walk(path):
@@ -19,19 +29,54 @@ def get_filenames(path, recursive):
                 yield path, f + "/"
             else:
                 yield path, f
-    
 
-def process_files(path, regex, rename, recursive):
+
+def get_file_renames(path, regex, rename, recursive):
     matcher = re.compile(regex)
     for dirpath, filename in get_filenames(path, recursive):
-        match = matcher.search(filename)
+        path = join(dirpath, filename)
+        match = matcher.search(path)
         if match:
             if rename:
-                new_name = rename.format(*match.groups())
-                os.rename(join(dirpath, filename), join(dirpath, new_name))
+                new_path = rename.format(*match.groups())
+                yield path, new_path
             else:
-                print(join(dirpath, filename))
-        
+                yield path
+
+
+def process_files(path, regex, rename, recursive, preview):
+    files = get_file_renames(path, regex, rename, recursive)
+
+    if not rename:
+        for filepath in files:
+            print(filepath)
+        return
+
+    if preview:
+        from itertools import chain, islice
+
+        if preview > 0:
+            to_preview = islice(files, preview)
+        else:
+            to_preview = files
+
+        previewed = []
+        for old_name, new_name in to_preview:
+            print(old_name, "=>", new_name)
+            previewed.append((old_name, new_name))
+
+        to_rename = chain(previewed, files)
+
+        should_commit = bool_input("Commit rename operations?")
+
+        if not should_commit:
+            return
+    else:
+        to_rename = files
+
+    for old_name, new_name in to_rename:
+        os.rename(old_name, new_name)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -44,7 +89,9 @@ if __name__ == "__main__":
             help="Whether or not to recursively search sub-directories.")
     parser.add_argument("--rename", type=str,
             help="Format string to rename matched files to. Use {n} for the nth capture group of the regex")
+    parser.add_argument("--preview", type=int, metavar="N",
+        nargs="?", const=-1, default=False,
+            help="Preview the frist n renames before commiting. If no number is given, all will be previewed.")
     args = parser.parse_args()
 
-    process_files(".", args.regex, args.rename, args.recursive)
-
+    process_files(".", args.regex, args.rename, args.recursive, args.preview)
