68e092cf7c191837f58bfb48d13045a1d6a0de91
Quinn Freedman <quinnfreedman@gmail.com>
2026-02-01 21:36:48 -0800

    remove zsh

--- .init_dotfiles.sh
+++ .init_dotfiles.sh
@@ -1,7 +1,2 @@
 alias config='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 config config --local status.showUntrackedFiles no
-
-plugins="$kak_config/plugins"
-mkdir -p "$plugins"
-[ ! -e "$plugins/plug.kak" ] && \
-    git clone -q https://github.com/andreyorst/plug.kak.git "$plugins/plug.kak"
